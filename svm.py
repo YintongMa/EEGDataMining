b223d4c785ed91732dac17da6f467c349f79d1b0
YintongMa <myt941126@gmail.com>
2021-05-28 16:23:25 -0500

    add rbf binary scale result && svm code refactor

--- svm.py
+++ svm.py
@@ -1,54 +1,6 @@
 import numpy as np
 from functools import lru_cache
 
-# class KP:
-#     def __init__(self):
-#         self._x = None
-#         self._alpha = self._b = self._kernel = None
-#
-#     @staticmethod
-#     def _poly(x, y, p=4):
-#         return (x.dot(y.T) + 1) ** p
-#
-#     @staticmethod
-#     def _rbf(x, y, gamma):
-#         return np.exp(-gamma * np.sum((x[..., None, :] - y) ** 2, axis=2))
-#
-#     def fit(self, x, y, kernel="rbf", p=None, gamma=None, c=1, lr=0.0001, batch_size=128, epoch=10000):
-#         x, y = np.asarray(x, np.float32), np.asarray(y, np.float32)
-#         if kernel == "poly":
-#             p = 4 if p is None else p
-#             self._kernel = lambda x_, y_: self._poly(x_, y_, p)
-#         elif kernel == "rbf":
-#             gamma = 1 / x.shape[1] if gamma is None else gamma
-#             self._kernel = lambda x_, y_: self._rbf(x_, y_, gamma)
-#         else:
-#             raise NotImplementedError("Kernel '{}' has not defined".format(kernel))
-#         self._alpha = np.zeros(len(x))
-#         self._b = 0.
-#         self._x = x
-#         k_mat = self._kernel(x, x)
-#         k_mat_diag = np.diag(k_mat)
-#         for _ in range(epoch):
-#             self._alpha -= lr * (np.sum(self._alpha * k_mat, axis=1) + self._alpha * k_mat_diag) * 0.5
-#             indices = np.random.permutation(len(y))[:batch_size]
-#             k_mat_batch, y_batch = k_mat[indices], y[indices]
-#             err = 1 - y_batch * (k_mat_batch.dot(self._alpha) + self._b)
-#             if np.max(err) <= 0:
-#                 continue
-#             mask = err > 0
-#             delta = c * lr * y_batch[mask]
-#             self._alpha += np.sum(delta[..., None] * k_mat_batch[mask], axis=0)
-#             self._b += np.sum(delta)
-#
-#     def predict(self, x, raw=False):
-#         x = np.atleast_2d(x).astype(np.float32)
-#         k_mat = self._kernel(self._x, x)
-#         y_pred = self._alpha.dot(k_mat) + self._b
-#         if raw:
-#             return y_pred
-#         return np.sign(y_pred).astype(np.float32)
-
 class svm():
 
     def __init__(self,
@@ -152,42 +104,3 @@ if __name__ == "__main__":
             cnt += 1
 
     print("cnt",cnt)
-
-
-    # xc, yc = gen_two_clusters()
-    # xc, yc = train_x, train_y
-    # kp = KP()
-    # kp.fit(xc, yc, kernel = "rbf", p=1)
-    # print("准确率：{:8.6} %".format((kp.predict(xc[:100,]) == yc[:100]).mean() * 100))
-    #
-
-
-    # print(cf.train(X,y))
-    # print(cf.fit(train_x, train_y))
-
-    # from sklearn.svm import SVC
-    # cf = SVC()
-    # cf.fit(train_x, train_y)
-    # p = cf.predict(train_x[12000:,])
-    # cnt = 0.
-    # for i,j in zip(p,train_y[12000:]):
-    #     print(i,j)
-    #     if i == j:
-    #         cnt += 1
-    #
-    # print("cnt",cnt)
-    # print(p)
-    # print("The score of linear is : %f" % cf.score(train_x,train_y))
-
-    #
-    # cf = CustomSVM()
-    # cf.fit(train_x[12000:], train_y[12000:])
-    # p = cf.predict((train_x[12000:]))
-    #
-    # cnt = 0.
-    # for i,j in zip(p,train_y[12000:]):
-    #     print(i,j)
-    #     if i == j:
-    #         cnt += 1
-    #
-    # print("cnt",cnt)
